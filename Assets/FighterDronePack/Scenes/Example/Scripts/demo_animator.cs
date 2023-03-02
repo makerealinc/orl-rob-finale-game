b66605ca215847aa3e34f5cc3a99b9c552408714
Jayden Diaz <jdiaz0705@live.com>
2023-03-02 11:11:33 -0500

    Add fighter drone asset

---
+++ Assets/FighterDronePack/Scenes/Example/Scripts/demo_animator.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:17533b9367ef0e7040df6532f63d6a0eba83e7fd0c1772fab7f0d54dd8fccb54
+size 6677
---
+++ Assets/FighterDronePack/Scenes/Example/Scripts/spin.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3d9e78b3e8e4c587ee641727784d263ac78334399e19d97411ed2ab85b38f89
+size 260
